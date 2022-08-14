5e03cffc2a713af6b381d04400e7c1c08dac5755
Ajay Sangwan <ajaysangwan122@gmail.com>
2022-08-14 15:51:24 -0400

    Add files via upload

---
+++ src/MethodDemo.java
@@ -0,0 +1,17 @@
+
+public class MethodDemo {
+
+	public static void main(String[] args) {
+		MethodDemo d = new MethodDemo();
+		d.getData();
+		MethodDemo2 d1 = new MethodDemo2();
+		d1.getUserData();
+
+	}
+	public void getData() {
+		
+		System.out.println("hello world");
+		
+	}
+
+}
